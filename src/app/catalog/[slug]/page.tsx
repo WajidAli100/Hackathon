f39af138cdfc3949f90b80e2b2f824dc61891912
WajidAli100 <68681790+WajidAli100@users.noreply.github.com>
2023-08-16 21:20:10 +0500

    issue solved

--- src/app/catalog/[slug]/page.tsx
+++ src/app/catalog/[slug]/page.tsx
@@ -1,11 +1,24 @@
+import { responseType } from "@/components/utils/ProductsDataArraysAndTypes";
+import ProductDetail from "@/components/views/ProductDetails/index";
+import ContextWrapper from "@/global/context/index";
+import { data } from "autoprefixer";
 import { Params } from "next/dist/shared/lib/router/utils/route-matcher"
 import { FC } from "react"
 import { Slug } from "sanity"
 
-const Catalog: FC<{ params: { Slug: string } }> = ({ params }) => {
+async function fetchPreviewData(slug: string) {
+    let res = await fetch(`https://70iorhhu.api.sanity.io/v2023-07-28/data/query/production?query=*%5B_type%3D%3D%22products%22+%26%26+slug.current%3D%3D%22${slug}%22%5D`)
+    return res.json();
+};
+
+const Catalog = async ({ params }: { params: { slug: string } }) => {
+    let data: responseType = await fetchPreviewData(params.slug)
     return (
         <div>
-            {params.Slug}
+            <ContextWrapper>
+                <ProductDetail item={data.result[0]} />
+                {/* {params.slug} */}
+            </ContextWrapper>
         </div>
     )
 }
